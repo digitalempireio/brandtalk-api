ca4214b1d2d03c1370f8a3cc061136a7d38a77a7
Mario Pabon <hey@mariopabon.com>
2015-04-16 16:52:58 -0400

    Search Route

--- routes/search.js
+++ routes/search.js
@@ -1,36 +1,17 @@
 var express = require('express'),
     https = require('https'),
-    search = express.Router();
-
-/*
-var twitter = require('ntwitter');
-
-var twit = new twitter({
-  consumer_key: 'lvhjUPGtVpuHzCax46CBtIo4c',
-  consumer_secret: 'NL2AeLwxiquiLLiFNQPEwZjrCg3K2SRtPLLaQrv8ClZahTKYo8',
-  access_token_key: '2704130948-wy84GX2Aygezenm4iQbfW9MCMXwttGnmxHMj2pT',
-  access_token_secret: 'epSZQ7VxVRVqnsB4JxVGrT5yBCJtKyX72S9F4Rm9DMzpQ'
-});
-
-twit.verifyCredentials(function(err, data){
-  //console.log(data);
-}).get('/search/tweets.json?q=micwosoft', {}, function(err, data) {
-  console.log(err);
-});*/
-
-var Twitter = require('twitter');
-
-var client = new Twitter(require('../env/credentials.js').twitter);
-
-var params = {q: 'microsoft'};
-client.get('search/tweets', params, function(error, tweets, response){
-  if (!error) {
-    console.log(tweets);
-  }
-});
+    search = express.Router(),
+    Twitter = require('twitter'),
+    client = new Twitter(require('../env/credentials.js').twitter);
 
 search.get('/:query', function(req, res){
-  res.json({ message: 'hewwo!'});
+
+  client.get('search/tweets', { q: req.params.query } , function(error, tweets, response){
+    if (!error) {
+      res.json(tweets);
+    }
+  });
+
 });
 
 module.exports = search;
