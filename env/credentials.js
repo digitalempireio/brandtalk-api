549658bc5a16112ac1762b2f30aa4834ac81acb2
Mario Pabon <hey@mariopabon.com>
2015-04-18 15:33:12 -0400

    Deleted env directory

--- env/credentials.js
+++
@@ -1,6 +0,0 @@
-module.exports.twitter = {
-  consumer_key: 'lvhjUPGtVpuHzCax46CBtIo4c',
-  consumer_secret: 'NL2AeLwxiquiLLiFNQPEwZjrCg3K2SRtPLLaQrv8ClZahTKYo8',
-  access_token_key: '2704130948-wy84GX2Aygezenm4iQbfW9MCMXwttGnmxHMj2pT',
-  access_token_secret: 'epSZQ7VxVRVqnsB4JxVGrT5yBCJtKyX72S9F4Rm9DMzpQ'
-};
